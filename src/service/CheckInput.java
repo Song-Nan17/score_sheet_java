2f58c33417c9325bdd0e16e75ed2ddfbf2ff8f2d
Song <songnan17@qq.com>
2019-03-10 21:39:55 +0800

    Add the logic of function isCorrectInfoFormat.

--- src/service/CheckInput.java
+++ src/service/CheckInput.java
@@ -1,7 +1,21 @@
 package service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class CheckInput {
     public static boolean isCorrectInfoFormat(String input) {
-        return true;
+        List<String> infos = Arrays.asList(input.split("，"));
+        boolean isCorrect = infos.size() >= 2 ? true : false;
+        if (infos.size() > 2) {
+            isCorrect = infos.stream().skip(2).map(s -> s.split("：")).allMatch(s -> s.length == 2 && isNumberic(s[1]));
+        }
+        return isCorrect;
+    }
+
+    public static boolean isNumberic(String str) {
+        Pattern pattern = Pattern.compile("-?[0-9]+\\.?[0-9]*");
+        return pattern.matcher(str).matches() ? true : false;
     }
 }
